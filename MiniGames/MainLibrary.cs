c390e31451f4dc5e1a7236a9f11be79a8b06bb9c
Rena <ckdwomn@live.com>
2019-09-15 22:47:43 +0900

    ver 0.1 alpha
    * 메인화면 구성

---
+++ MiniGames/MainLibrary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+using Windows.UI.Xaml.Controls;
+
+namespace MiniGames
+{
+    public class Menu
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string Value { get; set; }
+    }
+
+    class MainLibrary
+    {
+        /**
+         * 미니게임 메뉴 리스트
+         * 30개
+         */
+        public static List<Menu> Menus
+        {
+            get
+            {
+                return new List<Menu>()
+                {
+                    new Menu() { Name = "Noughts and Crosses", Icon = "\uE7FC", Value = "display" },
+                    new Menu() { Name = "Memory Game", Icon = "\uE7FC", Value = "notifications" },
+                    new Menu() { Name = "Light Game", Icon = "\uE7FC", Value = "batterysaver" },
+                    new Menu() { Name = "Order Game", Icon = "\uE7FC", Value = "storagesense" },
+                    new Menu() { Name = "Sound Game", Icon = "\uE7FC", Value = "datausage" },
+                    new Menu() { Name = "Lucky Bingo", Icon = "\uE7FC", Value = "personalization" },
+                    new Menu() { Name = "Hit or Miss", Icon = "\uE7FC", Value = "privacy" },
+                    new Menu() { Name = "Flag Game", Icon = "\uE7FC", Value = "developers" },
+                    new Menu() { Name = "Chase Game", Icon = "\uE7FC", Value = "developers" },
+                    new Menu() { Name = "Slide Game", Icon = "\uE7FC", Value = "developers" },
+                    new Menu() { Name = "High or Low", Icon = "\uE7FC", Value = "developers" },
+                    new Menu() { Name = "Racer Game", Icon = "\uE7FC", Value = "developers" },
+                    new Menu() { Name = "Emoji Game", Icon = "\uE7FC", Value = "developers" },
+                    new Menu() { Name = "Tiles Game", Icon = "\uE7FC", Value = "developers" },
+                    new Menu() { Name = "Lucky Wheel", Icon = "\uE7FC", Value = "developers" },
+                    new Menu() { Name = "Lucky Chess", Icon = "\uE7FC", Value = "developers" },
+                    new Menu() { Name = "Reversi", Icon = "\uE7FC", Value = "developers" },
+                    new Menu() { Name = "Mahjong", Icon = "\uE7FC", Value = "developers" },
+                };
+            }
+        }
+
+        public async void Launch(GridView display)
+        {
+            string value = ((Menu)(display.SelectedItem)).Value;
+            await Launcher.LaunchUriAsync(new Uri($"ms-settings:{value}"));
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# MiniGame ver.alpha
+## 소개
+이 저장소는 MiniGame UWP 개발 프로젝트용입니다.
+
+UWP용으로 개발된 미니 게임들을 즐길 수 있는 애플리케이션입니다.
+
+개인 프로젝트이기에 개발 진행속도가 느릴 수 있습니다.
+
+## 개발노트
+[위키로 이동하기...](https://github.com/Rena-Makise/MiniGameUWP/wiki)
+개발노트는 다음에서 볼 수 있습니다.
+
